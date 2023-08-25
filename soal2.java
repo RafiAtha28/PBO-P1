75c6e889a28fd2dad8d12e691182c2d12c45d34f
RafiAtha28 <98086522+RafiAtha28@users.noreply.github.com>
2023-08-25 23:35:24 +0700

    Add files via upload

---
+++ soal2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author 7270
+ */
+public class soal2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        int i = 51;
+        String s = (i<40)?"life":(i>50)?"universe":"everything";
+        System.out.println(s);
+    }
+}
